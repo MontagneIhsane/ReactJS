9d19dae4114607d90a0c144224e7f26f1132c269
MontagneIhsane <ihsane.montagne@gmail.com>
2025-02-27 14:04:33 +0100

    Add React Router + Change import

--- mon-projet/src/App.js
+++ mon-projet/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navigation from './Navigation';
-import PlantCollection from './PlantCollection';
+import Navigation from './components/Navbar';
+import PlantCollection from './components/PlantCollection';
 
 function App() {
   return (
